7058907aaf0a42ffd3a68f681d71b335e191e2d8
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-05-08 09:38:41 +0200

    minor changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { connectDB } = require ('./startup/db');
 connectDB();
 require('./startup/routes')(app);
 
-
+// to change the port just change the 8000 to any other port you would like.
 const port = process.env.PORT || 8000;
 app.listen(port, function(err){
     if (err) console.log("Error in server setup");
